ac88fd29889a0aaebe6d2d1b43979a5f7f04536f
albertodurante2 <34628237+albertodurante2@users.noreply.github.com>
2017-12-20 00:14:50 +0100

    Update CNMining.java

--- CNMining.java
+++ CNMining.java
@@ -706,8 +706,8 @@ public class CNMining
  
 		for (ObjectCursor<Node> e : endActs) {
                     Node temp = (Node)e.value;
-                        if ((temp.getInner_degree() > 0) {
-				Node en = cleanG.getNode(((temp).getNomeAttivita(), folded_map.get((temp).getNomeAttivita());
+                        if (temp.getInner_degree() > 0) {
+				Node en = cleanG.getNode(((temp).getNomeAttivita(), folded_map.get((temp).getNomeAttivita()));
 				endActivities.add(en);
 			}
 		}
